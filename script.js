38258e2543fdb82d2b3ec791418ae01208e19537
juniorpb2024 <jcjuniorm23@gmail.com>
2025-02-18 13:41:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ bulletPointsButton.addEventListener("click", () => {
         const range = selection.getRangeAt(0);
         const ul = doc.createElement("ul");
         const li = doc.createElement("li");
-        li.textContent = range.toString() || "•";
+        li.textContent = range.toString() ||;
         ul.appendChild(li);
         range.insertNode(ul);
 
